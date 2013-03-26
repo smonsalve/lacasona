b4089bae745f558ffb8007899ca6aae2f4afb5ed
jous32 <jous32@gmail.com>
2013-03-26 16:47:24 -0500

    sorry :) again 2

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -8,6 +8,6 @@ class TransactionController < ApplicationController
 #       property = Property.find(contract.property_id)
       #TO DO: Add  :invoice_number,:value,
       # Invoice.create(contract_id: => contract.id, invoice_date: => Date.today, invoice_status_id: , month: => Date.today.month,  :year => Date.today.year) 
-    end
+    # end
   end
 end
